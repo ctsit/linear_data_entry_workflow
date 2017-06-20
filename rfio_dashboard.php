ad2feee905b1662e96d7d1659e966c7543ab6968
Dileep <rajputd@ufl.edu>
2017-06-20 17:06:26 -0400

    Add rfio_dashboard.php

---
+++ rfio_dashboard.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Takes a json file and disables/enables certain forms for certain patients on
+ * record_status_dashboard based on the given json file.
+ *
+ */
+return function($project_id) {
+  
+}
+?>
