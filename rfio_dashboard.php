d0daf498de6d449ba89e96e20db61f7beacd5dd4
Dileep <rajputd@ufl.edu>
2017-06-21 16:25:33 -0400

    Remove irrelevant comment from rfio_dashboard

--- rfio_dashboard.php
+++ rfio_dashboard.php
@@ -1,9 +1,4 @@
 <?php
-/*
- * Takes a json file and disables/enables certain forms for certain patients on
- * record_status_dashboard based on the given json file.
- *
- */
 return function($project_id) {
 
 	$URL = $_SERVER['REQUEST_URI'];
