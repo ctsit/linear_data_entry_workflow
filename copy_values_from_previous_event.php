0c94ab8b8379415ca847cff996bc3ad7cfc79531
prasadlan <prasad2311@gmail.com>
2017-06-23 13:28:07 -0400

    fix to autofill fields with values only for new forms

--- copy_values_from_previous_event.php
+++ copy_values_from_previous_event.php
@@ -2,42 +2,43 @@
 	
 	return function($project_id){
 		include(dirname(__FILE__)."/../redcap_custom_project_settings/cps_lib.php");
-		//require_once "prefill_data.php";
-
-		if (!file_exists('../../redcap_connect.php')) {
-		    $REDCAP_ROOT = "/var/www/redcap";
-		    require_once $REDCAP_ROOT . '/redcap_connect.php';
-		} else {
-		    require_once '../../redcap_connect.php';
-		}
 
 		parse_str($_SERVER['QUERY_STRING'], $qs_params);
 		$pid = $qs_params['pid'];
 		$form_name = $qs_params['page'];
 		$event_id = $qs_params['event_id'];
-		/*
-			TODO: Check if record exists for $pid, $event_id, $field_name.
-		*/
+
 		$cps_lib = new cps_lib();
 		$fieldsObj = $cps_lib->getAttributeData($pid, 'copy_values_from_previous_event_hook');
 		$fieldsArr = json_decode($fieldsObj)->$form_name;
+
 		$result = array();
-		
 		$custom_data = REDCap::getData('json');
 		$encoded_data = json_decode($custom_data);
 		$max_event_id = 0;
+
 		foreach ($encoded_data as $item){
 			$unique_event_name = $item->redcap_event_name;
-			$event_id = REDCap::getEventIdFromUniqueEvent($unique_event_name);
-			if($event_id > $max_event_id){
+			$unique_event_id = REDCap::getEventIdFromUniqueEvent($unique_event_name);
+			if($unique_event_id > $max_event_id){
 				$result = array();
 				foreach ($fieldsArr as $field){
+					/*
+						Autofill fields with values in a form only if it is new form.
+					*/
+					if(isset($item->$field) && $event_id == $unique_event_id){
+						$result = array();
+						return;
+					}
+					/*
+						Get names and values of the fields that need to be autofilled.
+					*/
 					if(isset($item->$field)){
 						$latest_data_obj = new stdClass();
-						$max_event_id = $event_id;
+						$max_event_id = $unique_event_id;
 						$latest_data_obj->field_name = $field;
 						$latest_data_obj->value = $item->$field;
-						$latest_data_obj->event_id = $event_id;
+						$latest_data_obj->event_id = $unique_event_id;
 						$result[] = $latest_data_obj;
 					}
 				}
