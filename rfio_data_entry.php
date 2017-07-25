7015f246cd672c4113aca0133583505020073521
Dileep <rajputd@ufl.edu>
2017-07-25 17:12:06 -0400

    Stop rfio_data_entry.php from disabling the wrong instruments when called outside of the first event

--- rfio_data_entry.php
+++ rfio_data_entry.php
@@ -1,93 +1,92 @@
 <?php
+return function($project_id) {
 
-  return function ($project_id)
-  {
-    /* Code provided by Tiago that assembles a list of $selectors. The 
-    $selectors are gathered by starting from a list of all forms for the current
-    record and event. Then each form that is complete is removed, as well as the
-    first encountered non-complete (unverified or incomplete or new) form. The
-    remaining list contains only form IDs that need to be removed.
-    TL;DR: make $selectors, a string of form link IDs to be disabled*/
-    global $Proj;
+  //get form names used internally by REDCap
+  $forms = array_keys(REDCap::getInstrumentNames());
 
-    # Create $field array made of completion status fields of all project forms
-    /* Create $selectors array made of link IDs of all forms on left side
-    navigation panel (formMenuList)*/
-    $selectors = $fields = array();
-    foreach(array_keys($Proj->forms) as $form_name) {
-        $field_name = $form_name . '_complete';
-
-        $fields[] = $field_name;
-        $selectors[$field_name] = 'form[' . $form_name . ']';
-    }   
-
-    /* Use API to get array of completion status fields of all records
-    for the current event*/
-    $statuses = REDCap::getData($_GET['pid'], 'array', NULL,
-                $fields, $_GET['event_id']);
-
-
-    if (isset($statuses[$_GET['id']])) { # If current record is in $statuses
-      /* Set statuses as an array of the completion status fields for the
-      current record and event*/ 
-      $statuses = $statuses[$_GET['id']][$_GET['event_id']]; 
-
-      # Set flag for whether the current form should be included
-      $prev_is_complete = TRUE;
-      /* Loop through each form, exit loop if current form should not be
-      included*/
-      foreach ($statuses as $field_name => $status) {
-          if (!$prev_is_complete) {
-              break;
-          }   
-
-          # Remove current form from selectors if the previous form is complete
-          unset($selectors[$field_name]);
-          $prev_is_complete = ($status == 2);
-      }
-    }
-
-    # Remove current page's form from list of selectors.
-    unset($selectors[$_GET['page'] . '_complete']);
-    
-    # Our string of selectors, to be used/passed to Javascript.
-    $selectors = implode(', ', $selectors);
-    
-    
-    ?>
-      <script>
-        $(document).ready(function() {
-
-          // Generate array of form link IDs
-          var selector = '<?php echo $selectors; ?>'.split(", ");
-
-          // Function to disable use of target link
-          function disableLink(a) {
-            a.style.pointerEvents = 'none';
-            a.style.opacity = '.1';
-          }
-
-          function run() {
-            
-            // Loop through selector elements, find siblings
-            for (i = 0; i < selector.length; i++) {
-              var link = document.getElementById(selector[i]);
-              $siblings = $(link).siblings();
-
-              // If there is a sibling, disable its links
-              if($siblings.length !== 0) {
-                disableLink($siblings[0]);
-              }
-
-              // Disable link use of current selector element
-              disableLink(link);
-            }
-          }
-
-          run();
-        });
-      </script>
-    <?php
+  //use form names to contruct complete_status field names
+  foreach($forms as $index => $form_name) {
+    $forms[$index] = $form_name . '_complete';
   }
 
+  /*request data as an array to get corresponding record ids and events with
+  complete forms */
+  $completed_forms = REDCap::getData($_GET['pid'], 'array', $_GET['id'], $forms);
+
+  ?>
+
+    <script>
+
+    $('document').ready(function() {
+
+      var completedForms = <?php echo json_encode($completed_forms) ?>;
+
+      /*converts a pageName on a link to the corresponding form's complete_status
+      field name*/
+      function pageToFormComplete(pageName) {
+        return pageName + '_complete';
+      }
+
+      function disableForm(cell) {
+          cell.style.pointerEvents = 'none';
+          cell.style.opacity = '.1';
+      }
+
+      function getQueryString(url) {
+        url = decodeURI(url);
+        return url.match(/\?.+/)[0];
+      }
+
+      function getQueryParameters(url) {
+        var parameters = {};
+        var queryString = getQueryString(url);
+        var reg = /([^?&=]+)=?([^&]*)/g;
+        var keyValuePair;
+        while(keyValuePair = reg.exec(queryString)) {
+          parameters[keyValuePair[1]] = keyValuePair[2];
+        }
+        return parameters;
+      }
+
+      function run(){
+          var $links = $('.formMenuList');
+          var previousFormCompleted = true;
+
+          for(var i = 0; i < $links.length; i++) {
+            var childLinks = $links[i].querySelectorAll('a');
+
+            //if last form was incomplete disable every form after it
+            if(!previousFormCompleted) {
+              childLinks.forEach(function(url) {
+                disableForm(url);
+              });
+              continue;
+            }
+
+            for(var j = 0; j < childLinks.length; j++) {
+              var url = childLinks[j].href;
+              var param = getQueryParameters(url);
+
+              /*Need to check if completedForms value's are undefined because
+              REDcap does not enter data for incomplete/new forms.*/
+              if(completedForms[param.id] === undefined ||
+                 completedForms[param.id][param.event_id] === undefined ||
+                 completedForms[param.id][param.event_id][pageToFormComplete(param.page)] === undefined ||
+                 completedForms[param.id][param.event_id][pageToFormComplete(param.page)] !== "2") {
+                   previousFormCompleted = false;
+                   continue;
+              }
+            }
+          }
+      }
+
+      //run the hook
+      run();
+
+    });
+
+    </script>
+
+<?php
+}
 ?>
