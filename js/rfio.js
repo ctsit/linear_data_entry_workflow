9b37a125bd214996f93ed1fe1afcd427a7acf1a2
Tiago Bember Simeao <tbembersimeao@ufl.edu>
2017-08-29 15:10:02 -0400

    Refactoring RFIO js.

--- js/rfio.js
+++ js/rfio.js
@@ -1,5 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     var settings = linearDataEntryWorkflow.rfio;
+    var $links;
+
+    switch (settings.location) {
+        case 'data_entry_form':
+            hideNextFormButtons();
+            $links = $('.formMenuList a');
+            break;
+        case 'record_home':
+            $links = $('#event_grid_table a');
+            break;
+        case 'record_status_dashboard':
+            $links = $('#record_status_table a');
+            break;
+    }
+
+    if (typeof $links === 'undefined' || $links.lenght === 0) {
+        return false;
+    }
+
+    $links.each(function() {
+        if (this.href.indexOf(app_path_webroot + 'DataEntry/index.php?') === -1) {
+            return;
+        }
+
+        var params = getQueryParameters(this.href);
+        if (!settings.formsAccess[params.id][params.event_id][params.page]) {
+            disableForm(this);
+        }
+    });
 
     /**
      * Disables a link to a form.
@@ -35,20 +64,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     /**
-     * Checks access to a link and disables it, if needed.
+     * Hide "Save and Continue to Next Form" buttons.
      */
-    function checkLinkAccess(link) {
-        var params = getQueryParameters(link.href);
-        if (!settings.formsAccess[params.id][params.event_id][params.page]) {
-            disableForm(link);
-        }
-    }
-
-    /**
-     * Run RFIO for data entry forms.
-     */
-    function runDataEntryForm() {
-        // Hide "Save and Continue to Next Form" buttons.
+    function hideNextFormButtons() {
         var $buttons = $('button[name="submit-btn-savenextform"]');
 
         // Check if buttons are outside the dropdown menu.
@@ -72,72 +90,5 @@ document.addEventListener('DOMContentLoaded', function() {
             // Disable button inside the dropdown menu.
             $('a[onclick="dataEntrySubmit(\'submit-btn-savenextform\');return false;"]').hide();
         }
-
-        var $links = $('.formMenuList');
-        for (var i = 0; i < $links.length; i++) {
-            var childLinks = $links[i].querySelectorAll('a');
-
-            for (var j = 0; j < childLinks.length; j++) {
-                checkLinkAccess(childLinks[j]);
-            }
-        }
-    }
-
-    /**
-     * Run RFIO for record status dashboard page.
-     */
-    function runRecordStatusDashboard() {
-        var $rows = $('#record_status_table tbody tr');
-        if ($rows.length === 0) {
-            return false;
-        }
-
-        for (var i = 0; i < $rows.length; i++) {
-            // Start at 1 to avoid disabling Record ID column.
-            for (var j = 1; j < $rows[i].cells.length; j++) {
-                var link = $rows[i].cells[j].getElementsByTagName('a')[0];
-                checkLinkAccess(link);
-            }
-        }
-    }
-
-    /**
-     * Run RFIO for record home page.
-     */
-    function runRecordHome() {
-        var $rows = $('#event_grid_table tbody tr');
-        if ($rows.length === 0) {
-            return false;
-        }
-
-        // Start at 1 to avoid disabling "Data Collection Instrument"
-        // column.
-        for (var i = 1; i < $rows[0].cells.length; i++) {
-            for (var j = 0; j < $rows.length; j++) {
-                // Skips cells that do not have links or are members of the
-                // "Delete all data on event" row.
-                var link = $rows[j].cells[i].getElementsByTagName('a')[0];
-                if (link === undefined || $rows[j].cells[0].innerHTML === 'Delete all data on event:') {
-                    continue;
-                }
-
-                checkLinkAccess(link);
-            }
-        }
-    }
-
-    /**
-     * Main business logic.
-     */
-    switch (settings.location) {
-        case 'data_entry_form':
-            runDataEntryForm();
-            break;
-        case 'record_home':
-            runRecordHome();
-            break;
-        case 'record_status_dashboard':
-            runRecordStatusDashboard();
-            break;
     }
 });
