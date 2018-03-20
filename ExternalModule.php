5bc4177a0865f28a8d8ebf68c092bba94a526795
Tiago Bember Simeao <tbembersimeao@ufl.edu>
2018-03-20 12:12:10 -0400

    Enforcing consistency of buttons display.

--- ExternalModule.php
+++ ExternalModule.php
@@ -142,10 +142,28 @@ class ExternalModule extends AbstractExternalModule {
             'formsAccess' => $forms_access,
             'location' => $location,
             'instrument' => $instrument,
-            'isLastForm' => $Proj->lastFormName == $instrument,
-            'isException' => in_array($instrument, $exceptions),
+            'forceButtonsDisplay' => $Proj->lastFormName == $instrument ? 'show' : false,
             'hideNextRecordButton' => $this->getProjectSetting('hide-next-record-button', $Proj->project_id),
         );
+
+        if (!$settings['forceButtonsDisplay']) {
+            $i = array_search($instrument, $Proj->eventsForms[$event_id]);
+            $next_form = $Proj->eventsForms[$event_id][$i + 1];
+
+            if (in_array($next_form, $exceptions)) {
+                // Handling the case where the next form is an exception,
+                // so we need to show the buttons no matter the form status.
+                $settings['forceButtonsDisplay'] = 'show';
+            }
+            elseif (in_array($instrument, $exceptions)) {
+                if (!$forms_access[$record][$event_id][$next_form]) {
+                    // Handling the case where the current form is an exception
+                    // and the next one is not accessible, so we need to hide
+                    // the buttons no matter the form status.
+                    $settings['forceButtonsDisplay'] = 'hide';
+                }
+            }
+        }
 
         $this->setJsSetting('rfio', $settings);
         $this->includeJs('js/rfio.js');
--- js/rfio.js
+++ js/rfio.js
@@ -4,8 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     switch (settings.location) {
         case 'data_entry_form':
-            if (!settings.isLastForm && !settings.isException) {
-                hideNextFormButtons(settings.instrument, settings.hideNextRecordButton);
+            var $buttonsBottom = $('#__SUBMITBUTTONS__-div .btn-group');
+            var $buttonsTop = $('#formSaveTip .btn-group');
+
+            if (settings.hideNextRecordButton) {
+                // Hiding "Save & Go To Next Record" buttons.
+                removeButtons('savenextrecord');
+            }
+
+            if (settings.forceButtonsDisplay !== 'show') {
+                // Hiding "Save & Go to Next Form" buttons.
+                hideNextFormButtons(settings.instrument, settings.forceButtonsDisplay === 'hide');
             }
 
             $links = $('.formMenuList a');
@@ -67,39 +76,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     /**
-     * Hide "Save and Continue to Next Form" buttons.
+     * Hide "Save and Go to Next Form" buttons.
      */
-    function hideNextFormButtons(instrument, hideNextRecordButton) {
-        const FORM_STATUS_COMPLETE = '2';
-
-        var $complete = $('[name="' + instrument + '_complete"]');
-        var $buttonsBottom = $('#__SUBMITBUTTONS__-div .btn-group');
-        var $buttonsTop = $('#formSaveTip .btn-group');
-
-        if (hideNextRecordButton) {
-            // Hiding "Save & Go To Next Record" button.
-            removeButtons('savenextrecord');
-        }
-
-        // Storing original buttons markup.
-        var originalBottom = $buttonsBottom.html();
-        var originalTop = $buttonsTop.html();
-
-        // Checking initial form status.
-        if ($complete.val() !== FORM_STATUS_COMPLETE) {
-            removeButtons('savenextform');
-        }
-
-        // Dinamically remove or restore buttons according with form status.
-        $complete.change(function() {
-            if ($(this).val() === FORM_STATUS_COMPLETE) {
-                resetButtons();
-            }
-            else {
-                removeButtons('savenextform');
-            }
-        });
-
+    function hideNextFormButtons(instrument, force = false) {
         // Handling "Ignore and go to next form" button on required fields
         // dialog.
         $('#reqPopup').on('dialogopen', function(event, ui) {
@@ -115,44 +94,65 @@ document.addEventListener('DOMContentLoaded', function() {
             $(this).dialog('option', 'buttons', buttons);
         });
 
-        /**
-         * Removes the given submit buttons set.
-         */
-        function removeButtons(buttonName) {
-            var $buttons = $('button[name="submit-btn-' + buttonName + '"]');
-
-            // Check if buttons are outside the dropdown menu.
-            if ($buttons.length !== 0) {
-                $.each($buttons, function(index, button) {
-                    // Get first button in dropdown-menu.
-                    var replacement = $(button).siblings('.dropdown-menu').find('a')[0];
-
-                    // Modify button to behave like $replacement.
-                    button.id = replacement.id;
-                    button.name = replacement.name;
-                    button.onclick = replacement.onclick;
-                    button.innerHTML = replacement.innerHTML;
-
-                    // Get rid of replacement.
-                    $(replacement).remove();
-                });
-            }
-            else {
-                // Disable button inside the dropdown menu.
-                // Obs.: yes, this is a weird selector - "#" prefix is not being
-                // used - but this approach is needed on this page because there
-                // are multiple DOM elements with the same ID - which is
-                // totally wrong.
-                $('a[id="submit-btn-' + buttonName + '"]').hide();
-            }
+        if (force) {
+            removeButtons('savenextform');
+            return;
         }
 
-        /**
-         * Restores the original buttons.
-         */
-        function resetButtons() {
-            $buttonsBottom.html(originalBottom);
-            $buttonsTop.html(originalTop);
+        const FORM_STATUS_COMPLETE = '2';
+        var $complete = $('[name="' + instrument + '_complete"]');
+
+        // Storing original buttons markup.
+        var originalBottom = $buttonsBottom.html();
+        var originalTop = $buttonsTop.html();
+
+        // Checking initial form status.
+        if ($complete.val() !== FORM_STATUS_COMPLETE) {
+            removeButtons('savenextform');
+        }
+
+        // Dinamically remove or restore buttons according with form status.
+        $complete.change(function() {
+            if ($(this).val() === FORM_STATUS_COMPLETE) {
+                // Restoring buttons.
+                $buttonsBottom.html(originalBottom);
+                $buttonsTop.html(originalTop);
+            }
+            else {
+                removeButtons('savenextform');
+            }
+        });
+    }
+
+    /**
+     * Removes the given submit buttons set.
+     */
+    function removeButtons(buttonName) {
+        var $buttons = $('button[name="submit-btn-' + buttonName + '"]');
+
+        // Check if buttons are outside the dropdown menu.
+        if ($buttons.length !== 0) {
+            $.each($buttons, function(index, button) {
+                // Get first button in dropdown-menu.
+                var replacement = $(button).siblings('.dropdown-menu').find('a')[0];
+
+                // Modify button to behave like $replacement.
+                button.id = replacement.id;
+                button.name = replacement.name;
+                button.onclick = replacement.onclick;
+                button.innerHTML = replacement.innerHTML;
+
+                // Get rid of replacement.
+                $(replacement).remove();
+            });
+        }
+        else {
+            // Disable button inside the dropdown menu.
+            // Obs.: yes, this is a weird selector - "#" prefix is not being
+            // used - but this approach is needed on this page because there
+            // are multiple DOM elements with the same ID - which is
+            // totally wrong.
+            $('a[id="submit-btn-' + buttonName + '"]').hide();
         }
     }
 });
